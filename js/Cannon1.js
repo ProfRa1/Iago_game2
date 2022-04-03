f7d5e62cfed4401d543fc930253dcd7b615c171c
ProfRa1 <96995750+ProfRa1@users.noreply.github.com>
2022-04-03 10:02:04 -0300

    Add files via upload

---
+++ js/Cannon1.js
@@ -0,0 +1,25 @@
+class Cannon1 {
+    constructor(x, y, width, height, angle) {
+      this.x = x;
+      this.y = y;
+      this.width = width;
+      this.height = height;
+      this.angle = angle;
+      this.cannon_base = loadImage("assets/parte2.png");
+      
+    }
+    display() {
+      
+  
+      push();
+      translate(this.x, this.y);
+      rotate(this.angle);
+      imageMode(CENTER);
+      image(this.cannon_base, 0, 0, this.width, this.height);
+      pop();
+    
+  
+      noFill();
+    }
+  }
+  
